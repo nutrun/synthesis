dce5f564e147bded1fee313e4c1c490130d33f2f
George Malamidis <george@nutrun.com>
2008-05-31 14:35:28 +0100

    renamed cli expectation report format to text

---
+++ lib/synthesis/expectation_report_format.rb
@@ -0,0 +1,86 @@
+module Synthesis
+  module ExpectationReportFormat
+    module Text
+      def to_report
+        "(#{return_value_type}) " +
+        "#{receiver_repr}.#{@method}" + 
+        "(#{@args.map { |arg| arg.class } * ', '}) " + 
+        "in #{@track}"
+      end
+    end
+    
+    module Dot
+      #FIXME: move these somewhere else, probably where the formatter is loaded
+      require "parse_tree"
+      require "sexp"
+      require "sexp_processor"
+      
+      def to_report
+        "  \"#{discover_test_subject}\" -> " +
+        "\"#{receiver_class}\" "+
+        "[ label = "+
+        "\"(#{return_value_type}) "+
+        "#{method}(#{args.map { |arg| arg.class } * ', '})\" ];"
+      end
+            
+      private
+      
+      def discover_test_subject
+        if test_subject
+          filename, line, method = test_subject[1].split(':')
+          begin
+            method = method.scan(/`(.*)'/)[0][0]
+            ruby = File.read(filename)
+            parser = ParseTree.new
+            sexp = parser.parse_tree_for_string(ruby, filename).first
+            sexp = Sexp.from_array(sexp)
+            return DotProcessor.process(sexp, method)
+          rescue
+            return filename
+          end
+        else
+          return "?"
+        end
+      end
+
+      class DotProcessor < SexpProcessor
+        def self.process(exp, method)
+          analyzer = self.new
+          analyzer.method = method
+          analyzer.process(exp)
+          analyzer.klazz
+        end
+
+        attr_accessor :method
+        attr_reader :klazz
+
+        def initialize
+          super
+          self.strict = false
+          self.auto_shift_type = true
+          @ancestors = []
+        end
+
+        def process_module(exp)
+          name = exp.shift
+          @ancestors << name
+          s(:module, name, process(exp.shift))
+        end
+
+        def process_class(exp)
+          name = exp.shift
+          @ancestors << name
+          s(:class, name, exp.shift, process(exp.shift))
+        end
+
+        def process_defn(exp)
+          name = exp.shift
+          if name == method.to_sym
+            @klazz = @ancestors * '::'
+          end
+          s(:defn, name, process(exp.shift), process(exp.shift))
+        end
+      end
+    end
+  end
+end
