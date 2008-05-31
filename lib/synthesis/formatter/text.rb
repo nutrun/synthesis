9825b18150faa8946fbf639e287edd30af97cde6
George Malamidis <george@nutrun.com>
2008-05-31 15:31:15 +0100

    wrapping up text formatter

--- lib/synthesis/formatter/text.rb
+++ lib/synthesis/formatter/text.rb
@@ -3,19 +3,21 @@ module Synthesis
     include Logging
     
     def initialize
-      Expectation::Instance.send(:include, ExpectationReportFormat::Text)
-    end
-
-    def format_failure
-      ExpectationRecord.tested_expectations.each { |e| log e.to_report }
-      ExpectationRecord.untested_expectations.each { |e| log e.to_report }
-      log "Ignoring: #{ExpectationRecord.ignored.to_a * ', '}"
-      log; log "FAILED."
+      Expectation::Expectation.send(:include, ExpectationReportFormat::Text)
     end
     
     def format_success
       log; log "Verified #{ExpectationRecord.expectations.size} expectations"
       log "SUCCESS."
+    end
+
+    def format_failure
+      log; log "Tested Expectations: "
+      ExpectationRecord.tested_expectations.each { |e| log e.to_report }
+      log; log "Untested Expectations: "
+      ExpectationRecord.untested_expectations.each { |e| log e.to_report }
+      log "Ignoring: #{ExpectationRecord.ignored.to_a * ', '}"
+      log; log "FAILED."
     end
   end
 end
