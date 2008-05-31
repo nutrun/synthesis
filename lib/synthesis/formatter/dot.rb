0f8421b68fac2d7ec8eaf27266ffd40a7638580e
George Malamidis <george@nutrun.com>
2008-05-31 16:01:58 +0100

    cleanup

--- lib/synthesis/formatter/dot.rb
+++ lib/synthesis/formatter/dot.rb
@@ -4,29 +4,20 @@ module Synthesis
       Expectation::Expectation.send(:include, ExpectationReportFormat::Dot)
     end
     
-    def format_digraph
-      format_header
-      puts "  edge [color = green]"
-      ExpectationRecord.tested_expectations.each { |exp| puts exp.to_report  }
-      puts
-      puts "  edge [color = red]"
-      ExpectationRecord.untested_expectations.each  { |exp| puts exp.to_report  }
-      format_footer
-    end
-    alias format_failure format_digraph
-    alias format_success format_digraph
-    
-    private
-    def format_header
+    def graph
       puts "digraph synthesis_expectations {"
       puts "  rankdir=LR;"
       puts "  size=\"8,10\";"
       puts "  ratio=\"fill\";"
       puts "  node [shape = circle];"
-    end
-    
-    def format_footer
+      puts "  edge [color = green]"
+      ExpectationRecord.tested_expectations.each { |exp| puts exp.to_report  }
+      puts
+      puts "  edge [color = red]"
+      ExpectationRecord.untested_expectations.each  { |exp| puts exp.to_report  }
       puts "}"
     end
+    alias format_failure graph
+    alias format_success graph
   end
 end
