ed7707fbc9e9495c2cb8f0316a27a27ce2f659ed
Danilo Sato <DSato@thoughtworks.com>
2008-10-31 00:54:40 +0000

    Adding example tests with class methods

--- test_project/rspec/sample_spec.rb
+++ test_project/rspec/sample_spec.rb
@@ -9,6 +9,10 @@ module Source
     def do_once(data)
       @storage.save "#{data}"
     end
+    
+    def self.do_once(data)
+      Target.save "#{data}"
+    end
   end
 
   class Second
@@ -19,6 +23,10 @@ module Source
     def do_again(data)
       @storage.save "#{data}"
     end
+
+    def self.do_again(data)
+      Target.save "#{data}"
+    end
   end
 end
 
@@ -26,6 +34,10 @@ class Target
   def save(data)
     return false if @called
     @called = true
+  end
+  
+  def self.save(arg)
+    arg == 'rock'
   end
 end
 
@@ -45,5 +57,15 @@ describe "Scenarios for testing the dot reportter" do
     target = Target.new
     target.save('rock').should be_true
     target.save('rock').should be_false
+    
+    Target.save('rock').should be_true
+    Target.save('metal').should be_false
+  end
+
+  it "should cover class methods" do
+    Target.should_receive(:save).with('rock').and_return(true, false)
+
+    Source::First.do_once('rock').should be_true
+    Source::Second.do_again('rock').should be_false
   end
 end
